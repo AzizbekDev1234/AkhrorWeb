8b483fdf1f5008511a0ddc07e0188bbe03fd6def
Azizdev <a.s.abduhakimov1812@gmail.com>
2023-07-23 17:16:38 +0500

    first commit

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -55,8 +55,79 @@
 //  const func = calc(3 , 8)
 
 //  console.log(func);
-const calc = (a) => a ** 3;
 
-const func = calc(3);
+// const calc = (a) => a ** 3;
 
-console.log(func);
+// const func = calc(3);
+
+// console.log(func);
+
+// const greet = function () {
+//   return "Salom Hammaga"
+// }
+
+// const greetEl = greet()
+// console.log(greetEl)
+
+// const greet = (name) => {
+//   return `Salom ${name}`
+// }
+
+// const greetEl = greet("Azizbek")
+//  console.log(greetEl)
+
+// const soliq = function (max, pros) {
+//   const result = 0;
+//   for (let i = 0; i < max.length; i++) {
+//     result += max[i] + max[i] * pros;
+//   }
+//   return result;
+// };
+// const soqqa = soliq([23, 43, 54], 0.2);
+// console.log(soqqa);
+// const bill = function (products, tax) {
+//   const total = 0;
+
+//   for (let i = 0; i < products.length; i++) {
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// };
+// const result = bill([19, 12, 32], 0.2);
+// console.log(result);
+// ! Function vs Methods
+
+// callback
+// const func = (callback) => {
+//   let num = 39;
+//   callback(num);
+// };
+// func((result) => {
+//   console,log(result);
+// });
+// const myFunc = (callback) => {
+//   let number = 32
+//   callback(number),
+// };
+
+// myFunc((value) => {
+//   console.log(value)
+// })
+
+// ! forEach
+// const names = ["olma", "anor", "gilos", "qovun"];
+// const newArrays = []
+// names.forEach(function (item) {
+//   const newName = item.charAt().toUpperCase() + item.slice(1).toLowerCase() + "bek";
+//    newArrays.push(newName)
+// });
+
+// console.log(newArrays)
+
+// ! OBJECT
+const users = {
+  name: "Azizbek",
+  number: 930766923,
+  age: 19,
+  lang: uz,
+};
